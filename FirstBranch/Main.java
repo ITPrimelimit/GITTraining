eeacf56b30504705a0649f8425096c19de99869e
ananth0033 <b.ananth@gmail.com>
2018-12-10 07:48:18 +0530

    FirstBranch

---
+++ FirstBranch/Main.java
@@ -0,0 +1,18 @@
+class ProgrammingLanguage {
+  //attributes
+  String language_name;
+  String language_type;
+ 
+  //constructor
+  ProgrammingLanguage(String n, String t) {
+    language_name = n;
+    language_type = t;
+  }
+ 
+    ProgrammingLanguage Cpp = new ProgrammingLanguage("C++", "Object oriented");
+   
+    //calling method    
+    C.display();
+    Cpp.display();
+
+}
